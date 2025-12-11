394a9c0584b18153de8353e22981a148a9478c22
Kyran Detamore <shimmershine199@gmail.com>
2025-12-10 18:12:37 -0800

    Update main.js

--- Jscript/main.js
+++ Jscript/main.js
@@ -4,20 +4,15 @@ let total_count = 0;
     const PAGE_SIZE=25;
     let offset=0;
     function handleClickRandom() {
-        console.log("handleClickRandom called");   
-        const search = "api.giphy.com/v1/gifs/random"; 
         const elementOutputArea = document.getElementById("outputArea");
-        let url = `http://${search}?api_key=${API_KEY}`;
+        let url = `http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
         console.log('url=' + url);
 
         fetch(url, {method: "GET"})
             .then((response) => response.json())
             .then((json) => {
-                console.log(json);
                 const data = json.data;
-                console.log(data);
                 const url = json.data.images.fixed_height.url;
-                console.log(url);
                 let img=`<img src="${url}" alt="${data.title}" />`
                 elementOutputArea.innerHTML = `<div>${JSON.stringify(data.title)}</div>`;
                 elementOutputArea.innerHTML = img; 
@@ -25,17 +20,13 @@ let total_count = 0;
     }
 
     function handleClickArray() {
-        console.log("handleClickArray called");   
         const elementOutputArea = document.getElementById("outputArea");
         const search = document.getElementById("search").value;
         let url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}&limit=${PAGE_SIZE}&offset=${offset}`
-        console.log('url=' + url);
 
         fetch(url, {method: "GET"})
             .then((response) => response.json())
             .then((json) => {
-                console.log(json);
-                console.log(json.pagination);
                 total_count = json.pagination.total_count;
                 elementOutputArea.innerHTML = "";
                 for (let data of json.data) {
